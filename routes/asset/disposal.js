8b146210986046e66e50ad0be19fd9215a25939f
edwinwalela <edwinwalela@gmail.com>
2019-03-20 22:28:24 +0300

    disposal error checking

--- routes/asset/disposal.js
+++ routes/asset/disposal.js
@@ -38,7 +38,10 @@ Router.get('/:id',(req, res) => {
   let disposal = Disposal.findByPk(req.params.id,{include:[Asset]});
  
   Promise.all([disposal]).then(values=>{
-      res.send({disposal:values[0]});
+      if(values[0] !== null){
+        res.send({disposal:values[0]});
+      }
+      res.status(404).send({msg:"Not Found"});
   }).catch(err=>{
       res.status(500).send({});
   });
@@ -61,7 +64,10 @@ Router.put('/:id',(req,res)=>{
     });
 
     Promise.all([updateDisposal]).then(values=>{
-        res.status(200).send({msg:"OK"});
+        if(values[0] >= 1){
+            res.send({msg:"OK"}); 
+        }
+        res.status(404).send({msg:"Not Found"});
     }).catch(err=>{
         res.status(500).send({})
     });
@@ -75,7 +81,10 @@ Router.delete('/:id',(req,res)=>{
     });
 
     Promise.all([deleteDisposal]).then(values=>{
-        res.status(204).send({msg:"OK"});
+        if(values[0]>=1){
+            res.status(204).send({});
+        }
+        res.status(404).send({msg:"Not Found"});
     }).catch(err=>{
         res.status(500).send({})
     });
