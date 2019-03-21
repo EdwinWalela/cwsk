135c6fdab151317eefe96ba3e7260389e1b209bd
edwinwalela <edwinwalela@gmail.com>
2019-03-22 00:44:39 +0300

    enable jwt

--- routes/middleware/tokenVerification.js
+++ routes/middleware/tokenVerification.js
@@ -3,23 +3,23 @@ const jwtConfig = require("../../config/jwt");
 
 const verifyToken = (req,res,next) =>{
     const bearerHeader = req.headers["authorization"];
-    next();
+    
     // Check if undefined
-    // if(typeof bearerHeader !== "undefined"){
-    //     const bearerToken =  bearerHeader.split(" ")[1]
-    //     //decode token
-    //     let decoded = jwt.verify(bearerToken,jwtConfig.SECRET,(err,payload)=>{
-    //         if(err){
-    //             res.status(401).send({msg:err.message})
-    //         }else{
-    //             req.user = payload.user
-    //             next();
-    //         }
-    //     });
-    // }else{
-    //     //Unauthorized
-    //     res.status(401).send({msg:"Access token missing"})
-    // }
+    if(typeof bearerHeader !== "undefined"){
+        const bearerToken =  bearerHeader.split(" ")[1]
+        //decode token
+        let decoded = jwt.verify(bearerToken,jwtConfig.SECRET,(err,payload)=>{
+            if(err){
+                res.status(401).send({msg:err.message})
+            }else{
+                req.user = payload.user
+                next();
+            }
+        });
+    }else{
+        //Unauthorized
+        res.status(401).send({msg:"Access token missing"})
+    }
 }
 
 module.exports = verifyToken
