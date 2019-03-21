08a2061c15850def124df78008fc99e2a68ba60f
edwinwalela <edwinwalela@gmail.com>
2019-03-22 00:32:02 +0300

    remove view on index route

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,7 @@
 const Router = require("express").Router();
 
-//MODELS
-const Asset = require("../models/assets");
-const User = require("../models/users");
-const Role = require("../models/roles");
-const Type = require("../models/type");
-const Tps = require("../models/tps");
-const Insurance = require("../models/insurance");
-const Disposal = require("../models/disposal");
-const AssetValuation = require("../models/assetValuation");
-
-//CONFIGS
-const saltRounds = 10;
-
 Router.get('/',(req,res)=>{
-    res.render('site/index')
-});
-
-Router.get('/initialize',(req,res)=>{
-  User.create({
-    id: 1,
-    email: "admin@admin.com",
-    password: "1234",
-  }).then(data => {
-    res.json(data)
-  })
+    res.send('Hello World')
 });
 
 module.exports = Router;
