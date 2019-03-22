28b63ed648429ef50be6915543be3307806e7be8
edwinwalela <edwinwalela@gmail.com>
2019-03-23 00:00:53 +0300

    db config fix

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require('dotenv').config();
 // DB connection
 //
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.PASS,{
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host:process.env.DB_HOST,
     dialect:'postgres',
     operatorsAliases:false,
