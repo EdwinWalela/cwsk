8f3a3b545f64e80dfbad5dbd9992bde34ece216c
edwinwalela <edwinwalela@gmail.com>
2019-03-23 00:28:46 +0300

    db init middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const Router = require("express").Router();
+const DB_INIT = require('../routes/middleware/initializeDB');
 
 Router.get('/',(req,res)=>{
     res.send('Hello World')
 });
+
+Router.get('/init',DB_INIT,(req,res)=>{
+    res.send({msg:"OK"})
+})
 
 module.exports = Router;
---
+++ routes/middleware/initializeDB.js
@@ -0,0 +1,62 @@
+const Type = require("../../models/type");
+const TPS = require("../../models/tps");
+const Asset = require("../../models/assets");
+const Role = require("../../models/roles");
+const User = require("../../models/users");
+
+
+const dbInit = (req,res,next) =>{
+    let newTypeA = Type.create({
+        name:"orphanage"
+    })
+    let newTypeB = Type.create({
+        name:"vehical"
+    })
+    let newTps = TPS.create({
+        name:"Nairobi Children's Home",
+        alias:"NHC",
+        location:"Nairobi",
+        address:"Nairobi,Kenya",
+        phone:"0700000001",
+        typeId:1,
+        status:true,
+        description:"caters for children"
+    })
+    let newAsset = Asset.create({
+        name:"Ambulance",
+        pic:"",
+        tag:"",
+        cost:"2000000",
+        valuation:"2000000",
+        insurance:"Jubilee Insurance",
+        tpsId:1,
+        typeId:2
+    })
+    let newRole = Role.create({
+        name:"Admin"
+    })
+    let newUser = User.create({
+        firstName:"admin",
+        lastName:"admin",
+        alias:"admin",
+        phone:"",
+        email:"admin@mail.com",
+        idno:"10000000",
+        dob:"1/1/19",
+        resetCode:"",
+        permissions:"",
+        password:"pass",
+        confirmed:true,
+        roleId:1,
+        tpsId:1
+    });
+
+    Promise.all([newTypeA,newTypeB,newTps,newAsset,newRole,newUser]).then(values=>{
+        next();
+    }).catch(err=>{
+        res.status(500).send({err})
+    })
+
+}
+
+module.exports = dbInit
