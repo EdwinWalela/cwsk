2ae46b72a40320ef4832a729d1c70652ec64bb21
edwinwalela <edwinwalela@gmail.com>
2019-03-24 21:50:06 +0300

    remove db init

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -25,7 +25,7 @@ sequelize.authenticate()
     });
 
 sequelize.sync({
-  force:true  //  IF YOU SET THIS TO FALSE, IT WONT RECREATE THE DATABASE
+  force:false  //  IF YOU SET THIS TO FALSE, IT WONT RECREATE THE DATABASE
 });
 
 
